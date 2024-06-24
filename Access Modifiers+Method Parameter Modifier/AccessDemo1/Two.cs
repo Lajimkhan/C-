045368352c80f9115e958b19d22044d7c26b8bfc
MD. Ibrahim Khalil Lajim <102795066+Lajimkhan@users.noreply.github.com>
2024-06-24 20:48:12 +0600

    Add files via upload

---
+++ Access Modifiers+Method Parameter Modifier/AccessDemo1/Two.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessDemo1
+{
+    //Case 2: Child class from the same assembly
+    class Two:One
+    {
+        static void Main(string[] args)
+        {
+            Two.Test1(); Test3(); Test4(); Test5();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Access Modifiers+Method Parameter Modifier/MethodModifier/Class2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodModifier
+{
+    class Class2
+    {
+        public static void SimpleMethod(params int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i]);
+                Console.Write(", ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] x = new int[4] { 10, 12, 15, 17 };
+            int a = 20, b = 1988;
+            SimpleMethod(x);
+            SimpleMethod(a, b);
+            Console.ReadKey();
+        }
+    }
+}
