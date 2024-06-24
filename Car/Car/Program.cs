2ab077a376277f7c57b1c8057f639bf344053e75
MD. Ibrahim Khalil Lajim <102795066+Lajimkhan@users.noreply.github.com>
2024-06-24 20:46:15 +0600

    Add files via upload

---
+++ Car/Car/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+public class Vehicle
+{
+    public decimal Price { get; set; }
+    public string Color { get; set; }
+    public DateTime RegistrationDate { get; set; }
+    public DateTime PurchaseDate { get; set; }
+    public string FrameNo { get; set; }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        // Creating two objects of Vehicle class
+        var vehicle1 = new Vehicle()
+        {
+            Price = 25000.50m,
+            Color = "Red",
+            RegistrationDate = new DateTime(2020, 10, 1),
+            PurchaseDate = new DateTime(2021, 1, 15),
+            FrameNo = "X34RT67"
+        };
+
+        var vehicle2 = new Vehicle()
+        {
+            Price = 31000.75m,
+            Color = "Blue",
+            RegistrationDate = new DateTime(2019, 5, 20),
+            PurchaseDate = new DateTime(2020, 3, 10),
+            FrameNo = "Y89KL12"
+        };
+
+        // Printing the properties of the first vehicle
+        Console.WriteLine("Vehicle 1:");
+        Console.WriteLine("Price: {0:C}", vehicle1.Price);
+        Console.WriteLine("Color: {0}", vehicle1.Color);
+        Console.WriteLine("Registration Date: {0:d}", vehicle1.RegistrationDate);
+        Console.WriteLine("Purchase Date: {0:d}", vehicle1.PurchaseDate);
+        Console.WriteLine("Frame No: {0}", vehicle1.FrameNo);
+        Console.WriteLine();
+
+        // Printing the properties of the second vehicle
+        Console.WriteLine("Vehicle 2:");
+        Console.WriteLine("Price: {0:C}", vehicle2.Price);
+        Console.WriteLine("Color: {0}", vehicle2.Color);
+        Console.WriteLine("Registration Date: {0:d}", vehicle2.RegistrationDate);
+        Console.WriteLine("Purchase Date: {0:d}", vehicle2.PurchaseDate);
+        Console.WriteLine("Frame No: {0}", vehicle2.FrameNo);
+        Console.WriteLine();
+        Console.ReadKey
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* string fuckerName = "Bal";
+             int fuckerAge;
+             fuckerAge = 100;
+             Console.WriteLine(fuckerName);
+             Console.WriteLine("Hello World");
+             Console.WriteLine("Fucker Age "+fuckerAge);
+             fuckerName="@Hoga";
+
+             Console.WriteLine("The name is "+ fuckerName);*/
+            string mFuckr;
+            mFuckr= "ChudirVai";
+            Console.WriteLine(mFuckr);
+            char Hala = Convert.ToChar(69);
+            Console.WriteLine(Hala);
+
+            Console.ReadLine();
+
+        }
+    }
+}
